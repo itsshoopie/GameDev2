1ca7e6d47fa781433284eff0637e2996c3d3b0a3
Shoopie <silvershoopie@gmail.com>
2024-02-26 16:09:12 -0500

    finished pistol
    added fire rate, automatic, and spread

--- First Person Shooter/Assets/Player/Guns/GunData.cs
+++ First Person Shooter/Assets/Player/Guns/GunData.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 [CreateAssetMenu(fileName = "GunData", menuName = "GunData")]
 public class GunData : ScriptableObject
 {
     public float range = 1000f;
+    public int ammo_per_clip = 12;
+    public bool automatic = false;
+    public float primary_fire_delay = 0.5f;
+    [Range(0f, 90f)] public float spread = 0.0f;
 }
